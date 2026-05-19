780557061558e066c0b022719275966c9d286fd2
qingyeastr <qingyeastr@163.com>
2026-05-19 17:41:38 +0800

    git init

---
+++ src/test/java/com/qingyun/javakqa/JavaKqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qingyun.javakqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
